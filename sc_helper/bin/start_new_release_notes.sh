feef2f4511d116a916d9da70a5e0c4c257c064e8
Jonathan Manning <jmanning@ebi.ac.uk>
2021-03-04 17:47:04 +0000

    Clean up interface for SCXA release notes script

--- sc_helper/bin/start_new_release_notes.sh
+++ sc_helper/bin/start_new_release_notes.sh
@@ -1,13 +1,37 @@
 #!/usr/bin/env bash
 
-# Override release date
-# export releaseDate=2020-07-08
-releaseDate=${releaseDate:-$(date "+%Y-%m-%d")}
-# Override release number:
-# export releaseNumber=13
-# Override last release date by exporting outside:
-# export PREVIOUS_RELEASE_DATE=2020-12-03
+usage() { echo "Usage: $0 [-d <release date in +%Y-%m-%d, guessed from current date if not set>] [-r <release number, guessed from last release if not set>] [-p <previous release date to compare to, in +%Y-%m-%d, guessed fromlast release if not set>] -a <atlas sc experiments dir, defaults to value of ATLAS_SC_EXPERIMENTS>" 1>&2; }
 
+# Parse arguments
+
+releaseDate=${releaseDate:-$(date "+%Y-%m-%d")}
+releaseNumber=
+previousReleaseDate=
+atlasScExperimentsDir=${ATLAS_SC_EXPERIMENTS:-''}
+
+while getopts ":d:r:p:" o; do
+    case "${o}" in
+        d)
+            releaseDate=${OPTARG}
+            ;;
+        r)
+            releaseNumber=${OPTARG}
+            ;;
+        p)
+            previousReleaseDate=${OPTARG}
+            ;;
+        a)
+            atlasScExperimentsDir=${OPTARG}
+            ;;
+        *)
+            usage
+            exit 0
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+# Guess release number from last release
 
 if [ -z "$releaseNumber" ]; then
     lastRelease=$(ls release-notes/sc/_posts/ | sed 's/.md//' | awk -F'-' '{print $4}' | sort -nr | head -n 1)
@@ -17,21 +41,23 @@ fi
 releaseNotesFile="release-notes/sc/_posts/${releaseDate}-${releaseNumber}.md"
 echo "Will write new release notes to release-notes/sc/_posts/${releaseDate}-${releaseNumber}.md"
 
-if [ -z "$ATLAS_SC_EXPERIMENTS" ]; then
-    echo "You need to set ATLAS_SC_EXPERIMENTS" 1>&2
+if [ -z "$atlasScExperimentsDir" ]; then
+    echo "You need to set atlasScExperimentsDir, or the ATLAS_SC_EXPERIMENTS environment variable." 1>&2
     exit 1
 fi
 
-raw_cells=$(printf "%'.f" $(cat $ATLAS_SC_EXPERIMENTS/cell_stats.json | jq '.raw_cells'))
-filtered_cells=$(printf "%'.f" $(cat $ATLAS_SC_EXPERIMENTS/cell_stats.json | jq '.filtered_cells'))
+raw_cells=$(printf "%'.f" $(cat $atlasScExperimentsDir/cell_stats.json | jq '.raw_cells'))
+filtered_cells=$(printf "%'.f" $(cat $atlasScExperimentsDir/cell_stats.json | jq '.filtered_cells'))
 all_species=`curl -s https://wwwdev.ebi.ac.uk/gxa/sc/json/experiments | jq '.experiments | .[].species' | sort -u`
 n_species=$(echo -e "$all_species" | tr '[:upper:]' '[:lower:]' | wc -l)
 n_studies=$(curl -s https://wwwdev.ebi.ac.uk/gxa/sc/json/experiments | jq '.experiments | map(select(.rawExperimentType | test("(BASELINE)"; "i"))) | length')
-last_release_epoch_time=$(ls -t release-notes/sc/_posts/ | sed 's/.md//' | awk -F'-' '{print $2"/"$3"/"$1}' | while read -r l; do date "+%s" -d "$l"; done | sort -nr | head -n 1)
 
-if [ -n "$PREVIOUS_RELEASE_DATE" ]; then
-    # override lastRelease
-    last_release_epoch_time=$(echo $PREVIOUS_RELEASE_DATE | awk -F'-' '{ print $2"/"$3"/"$1}' | while read -r l; do date "+%s" -d "$l"; done | sort -nr | head -n 1)
+# Guess last release date if not provided
+
+if [ -n "$previousReleaseDate" ]; then
+    last_release_epoch_time=$(echo $previousReleaseDate | awk -F'-' '{ print $2"/"$3"/"$1}' | while read -r l; do date "+%s" -d "$l"; done | sort -nr | head -n 1)
+else
+    last_release_epoch_time=$(ls -t release-notes/sc/_posts/ | sed 's/.md//' | awk -F'-' '{print $2"/"$3"/"$1}' | while read -r l; do date "+%s" -d "$l"; done | sort -nr | head -n 1)
 fi
 
 # Replace wildcards in stats with auto-derived versions
