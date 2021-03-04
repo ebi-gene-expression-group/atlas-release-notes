7ebdecb80a2440e8b81d2a401021cceb90c9510a
Pablo Moreno <pcm32@users.noreply.github.com>
2021-03-04 15:43:24 +0000

    Enables user to set previous release date (#24)
    * Update start_new_release_notes.sh
    
    * All vars injected as env vars
    
    * shorter test
    
    Co-authored-by: Jonathan Manning <jmanning@ebi.ac.uk>
    
    Co-authored-by: Jonathan Manning <jmanning@ebi.ac.uk>

--- sc_helper/bin/start_new_release_notes.sh
+++ sc_helper/bin/start_new_release_notes.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-releaseDate=${1:-$(date "+%Y-%m-%d")}
-releaseNumber=${2}
+# Override release date
+# export releaseDate=2020-07-08
+# Override release number:
+# export releaseNumber=13
+# Override last release date by exporting outside:
+# export PREVIOUS_RELEASE_DATE=2020-12-03
+
 
 if [ -z "$releaseNumber" ]; then
     lastRelease=$(ls release-notes/sc/_posts/ | sed 's/.md//' | awk -F'-' '{print $4}' | sort -nr | head -n 1)
@@ -23,6 +28,11 @@ n_species=$(echo -e "$all_species" | tr '[:upper:]' '[:lower:]' | wc -l)
 n_studies=$(curl -s https://wwwdev.ebi.ac.uk/gxa/sc/json/experiments | jq '.experiments | map(select(.rawExperimentType | test("(BASELINE)"; "i"))) | length')
 last_release_epoch_time=$(ls -t release-notes/sc/_posts/ | sed 's/.md//' | awk -F'-' '{print $2"/"$3"/"$1}' | while read -r l; do date "+%s" -d "$l"; done | sort -nr | head -n 1)
 
+if [ -n "$previousReleaseDate" ]; then
+    # override lastRelease
+    last_release_epoch_time=$(echo $PREVIOUS_RELEASE_DATE | awk -F'-' '{ print $2"/"$3"/"$1}' | while read -r l; do date "+%s" -d "$l"; done | sort -nr | head -n 1)
+fi
+
 # Replace wildcards in stats with auto-derived versions
 
 cat sc_helper/release_notes_templates/data_statistics.md | \
@@ -37,6 +47,5 @@ curl 'https://wwwdev.ebi.ac.uk/gxa/sc/json/experiments' |
     awk -v FS="," '{ printf "- [%s](https://www.ebi.ac.uk/gxa/sc/experiments/%s)\n", $2, $1}' | sed s/\"//g >> $releaseNotesFile
 
 echo -e "\n#### New features" >> $releaseNotesFile
-
 
 
